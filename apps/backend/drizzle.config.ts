172ad21d1682fee75cb2ec6014bd24015e0c69db
joko <156732134+jskn-d@users.noreply.github.com>
2025-04-07 01:45:57 +0900

    コメントアウト削除

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -5,9 +5,4 @@ export default {
     schema: './src/database/schema.ts',
     dialect: 'sqlite',
     driver: 'd1-http',
-    // dbCredentials: {
-    //     accountId: env<{ CLOUDFLARE_ACCOUNT_ID: string }>(Context),
-    //     // databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    //     // token: process.env.CLOUDFLARE_D1_TOKEN!,
-    // },
 } satisfies Config;
